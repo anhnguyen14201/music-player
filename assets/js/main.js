b169bbbec270906d25b16d9a345912a2e79270d2
Anh Nguyen <69924204+anhnguyen14201@users.noreply.github.com>
2022-12-22 20:15:37 +0700

    Update main.js
    Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,114 +60,114 @@ const app = {
     {
       name: "Mashup Nevada x Di Di Di",
       singer: "Daniel Mastro",
-      path: "/assets/music/Mashup Nevada x Di Di Di - Daniel Mastro.mp3",
-      image: "/assets/img/dididixnevada.jpg",
+      path: "assets/music/Mashup Nevada x Di Di Di - Daniel Mastro.mp3",
+      image: "assets/img/dididixnevada.jpg",
     },
 
     {
       name: "Nevada",
       singer: "Vicetone Cozi Zuehlsdorff",
-      path: "/assets/music/Nevada - Vicetone_ Cozi Zuehlsdorff.mp3",
-      image: "/assets/img/nevada.jpg",
+      path: "assets/music/Nevada - Vicetone_ Cozi Zuehlsdorff.mp3",
+      image: "assets/img/nevada.jpg",
     },
 
     {
       name: "Nơi này có anh piano cover",
       singer: "An Coong",
-      path: "/assets/music/NoiNayCoAnhPianoCover-AnCoong-4780660.mp3",
-      image: "/assets/img/nơi-nay-co-anh-piano.jpg",
+      path: "assets/music/NoiNayCoAnhPianoCover-AnCoong-4780660.mp3",
+      image: "assets/img/nơi-nay-co-anh-piano.jpg",
     },
 
     {
       name: "Reality",
       singer: "Lost Frequencies",
-      path: "/assets/music/Reality - Lost Frequencies_ Janieck Devy.mp3",
-      image: "/assets/img/reality.jpg",
+      path: "assets/music/Reality - Lost Frequencies_ Janieck Devy.mp3",
+      image: "assets/img/reality.jpg",
     },
 
     {
       name: "Set fire to the rain remix",
       singer: "Adele",
-      path: "/assets/music/Set Fire To The Rain Remix_ - Adele.mp3",
-      image: "/assets/img/set-fire-to-the-rain.jpg",
+      path: "assets/music/Set Fire To The Rain Remix_ - Adele.mp3",
+      image: "assets/img/set-fire-to-the-rain.jpg",
     },
 
     {
       name: "Sugar",
       singer: "Maroon 5",
-      path: "/assets/music/Sugar - Maroon 5.mp3",
-      image: "/assets/img/sugar.jfif",
+      path: "assets/music/Sugar - Maroon 5.mp3",
+      image: "assets/img/sugar.jfif",
     },
 
     {
       name: "Mashup x Something just like this",
       singer: "WilliamYT",
-      path: "/assets/music/y2mate.com -  Tik Tok  Umbrella x Something Just Like This x Take A While Remix  Nhạc Mashup Tik Tok Cực Hay.mp3",
+      path: "assets/music/y2mate.com -  Tik Tok  Umbrella x Something Just Like This x Take A While Remix  Nhạc Mashup Tik Tok Cực Hay.mp3",
       image:
-        "/assets/img/Umbrella x Something Just Like This x Take A While Remix.jpg",
+        "assets/img/Umbrella x Something Just Like This x Take A While Remix.jpg",
     },
 
     {
       name: "Mood",
       singer: "24kFoldn",
-      path: "/assets/music/y2mate.com - 24kGoldn  Mood Official Video ft iann dior.mp3",
-      image: "/assets/img/mood.jpg",
+      path: "assets/music/y2mate.com - 24kGoldn  Mood Official Video ft iann dior.mp3",
+      image: "assets/img/mood.jpg",
     },
 
     {
       name: "Despair Remix",
       singer: "SeVen.13",
-      path: "/assets/music/y2mate.com - Despair Remix  风靡全网的背景  Tik Tok  抖音 DouYin  Bài Hát Hot Trên TikTok Trung Quốc.mp3",
-      image: "/assets/img/despair.jpg",
+      path: "assets/music/y2mate.com - Despair Remix  风靡全网的背景  Tik Tok  抖音 DouYin  Bài Hát Hot Trên TikTok Trung Quốc.mp3",
+      image: "assets/img/despair.jpg",
     },
 
     {
       name: "Funk you",
       singer: "EA7",
-      path: "/assets/music/y2mate.com - Funk You Extended Mix  EA7 Funk You五四蹦迪第一弹  Nhạc Nên Tik Tok  Tik Tok  抖音 Douyin.mp3",
-      image: "/assets/img/funk-you.jpg",
+      path: "assets/music/y2mate.com - Funk You Extended Mix  EA7 Funk You五四蹦迪第一弹  Nhạc Nên Tik Tok  Tik Tok  抖音 Douyin.mp3",
+      image: "assets/img/funk-you.jpg",
     },
 
     {
       name: "Larg",
       singer: "Elgit Doada",
-      path: "/assets/music/y2mate.com - Larg.mp3",
-      image: "/assets/img/larg.jpg",
+      path: "assets/music/y2mate.com - Larg.mp3",
+      image: "assets/img/larg.jpg",
     },
 
     {
       name: "Harehare Ya",
       singer: "Kiyod",
-      path: "/assets/music/y2mate.com - Lyrics ハレハレヤHarehare Ya  Cover by Kityod猫瑾醒了吗 full.mp3",
-      image: "/assets/img/stream-hare-hare-ya.jpg",
+      path: "assets/music/y2mate.com - Lyrics ハレハレヤHarehare Ya  Cover by Kityod猫瑾醒了吗 full.mp3",
+      image: "assets/img/stream-hare-hare-ya.jpg",
     },
 
     {
       name: "Ông trời làm tội anh chưa Beat",
       singer: "MINH HANH x RASTZ x QNT ft TUẤN CRY",
-      path: "/assets/music/y2mate.com - ÔNG TRỜI LÀM TỘI ANH CHƯA  MINH HANH x RASTZ x QNT ft TUẤN CRY INSTRUMENTAL.mp3",
-      image: "/assets/img/ong-troi-lam-toi-anh-chua.jpg",
+      path: "assets/music/y2mate.com - ÔNG TRỜI LÀM TỘI ANH CHƯA  MINH HANH x RASTZ x QNT ft TUẤN CRY INSTRUMENTAL.mp3",
+      image: "assets/img/ong-troi-lam-toi-anh-chua.jpg",
     },
 
     {
       name: "Love is gone cover",
       singer: "Slander",
-      path: "/assets/music/y2mate.com - SLANDER  Love Is Gone ft Dylan Matthew Acoustic.mp3",
-      image: "/assets/img/love-is-gone.jpg",
+      path: "assets/music/y2mate.com - SLANDER  Love Is Gone ft Dylan Matthew Acoustic.mp3",
+      image: "assets/img/love-is-gone.jpg",
     },
 
     {
       name: "You Don't Know Me",
       singer: "Ofenbach",
-      path: "/assets/music/y2mate.com - VietsubLyrics You Dont Know Me  Ofenbach Brodie Barclay.mp3",
-      image: "/assets/img/you-dont-no-me.jpg",
+      path: "assets/music/y2mate.com - VietsubLyrics You Dont Know Me  Ofenbach Brodie Barclay.mp3",
+      image: "assets/img/you-dont-no-me.jpg",
     },
 
     {
       name: "MiMiMi",
       singer: "Dramma",
-      path: "/assets/music/y2mate.com - 抖音Tiktok Dramma  МиМиМи Mimimi  Bài hát hot Tiktok.mp3",
-      image: "/assets/img/mimimi.jpg",
+      path: "assets/music/y2mate.com - 抖音Tiktok Dramma  МиМиМи Mimimi  Bài hát hot Tiktok.mp3",
+      image: "assets/img/mimimi.jpg",
     },
   ],
 
